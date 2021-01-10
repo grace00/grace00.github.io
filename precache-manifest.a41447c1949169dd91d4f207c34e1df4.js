33eb3f958b5733562d3031626e54ccf722263dd7
Grace <gkzhou@stanford.edu>
2021-01-10 18:52:18 -0500

    Updates

---
+++ precache-manifest.a41447c1949169dd91d4f207c34e1df4.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ff6633abbce40428ad1f68db261a8f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0c89ceda517d93159fe0",
+    "url": "/portfolio/static/css/main.bb46743e.chunk.css"
+  },
+  {
+    "revision": "fd4116e5c5c0ec18e62a",
+    "url": "/portfolio/static/js/2.ab357a33.chunk.js"
+  },
+  {
+    "revision": "0c89ceda517d93159fe0",
+    "url": "/portfolio/static/js/main.5e6f4f2d.chunk.js"
+  },
+  {
+    "revision": "9916aa5016a36c80a421",
+    "url": "/portfolio/static/js/runtime-main.f84e4eb0.js"
+  },
+  {
+    "revision": "14af594234516c4b294caa93beff650a",
+    "url": "/portfolio/static/media/CS206_Paper.14af5942.pdf"
+  },
+  {
+    "revision": "7753d8fc4a2716d27f4789e006176ada",
+    "url": "/portfolio/static/media/Final_Demo.7753d8fc.mp4"
+  },
+  {
+    "revision": "22b03cd86d883ffb7a9c013835142109",
+    "url": "/portfolio/static/media/advice.22b03cd8.png"
+  },
+  {
+    "revision": "8389613a5c154156d4ea098de261b5c7",
+    "url": "/portfolio/static/media/advicewireframe.8389613a.png"
+  },
+  {
+    "revision": "dd970b474ff0a25e882476a6b7612304",
+    "url": "/portfolio/static/media/audio.dd970b47.png"
+  },
+  {
+    "revision": "57396d35a8f4af0dcaa4bd3cb7afa76c",
+    "url": "/portfolio/static/media/classifieds.57396d35.jpg"
+  },
+  {
+    "revision": "04d768a1e99890fb7918993bd9c0d7da",
+    "url": "/portfolio/static/media/detailswireframe.04d768a1.png"
+  },
+  {
+    "revision": "167d406703b824a73b03f75228086ec4",
+    "url": "/portfolio/static/media/genz-mock@2x.167d4067.png"
+  },
+  {
+    "revision": "9f6c587f56bf89fc0690a9825ed39f24",
+    "url": "/portfolio/static/media/jobcompetitors.9f6c587f.png"
+  },
+  {
+    "revision": "109264d4da54b466317fb6e6d7a55411",
+    "url": "/portfolio/static/media/jobcomputer@2x.109264d4.png"
+  },
+  {
+    "revision": "4db644fc97ee5b88e936c0a77e6a15eb",
+    "url": "/portfolio/static/media/jobdetails.4db644fc.png"
+  },
+  {
+    "revision": "4cf2b99782c8061f0dec6eb725ad7b62",
+    "url": "/portfolio/static/media/jobflow.4cf2b997.png"
+  },
+  {
+    "revision": "9add8a30eb515e6f84a7721781987356",
+    "url": "/portfolio/static/media/jobmockup@2x.9add8a30.png"
+  },
+  {
+    "revision": "fc7926828e282c631824299b211ebc6d",
+    "url": "/portfolio/static/media/jobs-site-map.fc792682.jpg"
+  },
+  {
+    "revision": "5ac9b43f846be260b5aa867d38607719",
+    "url": "/portfolio/static/media/jobslist.5ac9b43f.png"
+  },
+  {
+    "revision": "3ac91777afd903999ccd1ce25b018bf6",
+    "url": "/portfolio/static/media/jobwireframe.3ac91777.png"
+  },
+  {
+    "revision": "fc38b28d5f48002cc7e38f437d5aec15",
+    "url": "/portfolio/static/media/journie-blueprint.fc38b28d.png"
+  },
+  {
+    "revision": "5efd38d7df6279634669229996ca8043",
+    "url": "/portfolio/static/media/journie-brainstorm.5efd38d7.png"
+  },
+  {
+    "revision": "a58a41fc3c61c381a3573699f86f8856",
+    "url": "/portfolio/static/media/journie-calendar.a58a41fc.png"
+  },
+  {
+    "revision": "1cbb24e314d6f8d87ca3a1fff91684f7",
+    "url": "/portfolio/static/media/journie-comparative.1cbb24e3.png"
+  },
+  {
+    "revision": "5a3b92183b07a3cbfb94a1b01f548319",
+    "url": "/portfolio/static/media/journie-comparator-guide.5a3b9218.png"
+  },
+  {
+    "revision": "ded523938ba986b2b1d46cf99bf7801d",
+    "url": "/portfolio/static/media/journie-flow.ded52393.png"
+  },
+  {
+    "revision": "5471b3142a04f3e7a3ccffbf7753391a",
+    "url": "/portfolio/static/media/journie-info.5471b314.gif"
+  },
+  {
+    "revision": "d7a801173a6abe430c98d39c5a4be4ca",
+    "url": "/portfolio/static/media/journie-info.d7a80117.png"
+  },
+  {
+    "revision": "2517c42852835153caf4807473d7a315",
+    "url": "/portfolio/static/media/journie-interview-guide.2517c428.png"
+  },
+  {
+    "revision": "9fa5bfbfab0b5227f48fbb2d4b54b9f7",
+    "url": "/portfolio/static/media/journie-interview-notes.9fa5bfbf.png"
+  },
+  {
+    "revision": "012ba9499995fe21d40775701e358ae2",
+    "url": "/portfolio/static/media/journie-join.012ba949.gif"
+  },
+  {
+    "revision": "f76a87d0ba7b60e9868fc7d274c8ae34",
+    "url": "/portfolio/static/media/journie-journey-map.f76a87d0.png"
+  },
+  {
+    "revision": "cfd5754b196605ffb301ea1b788eadbe",
+    "url": "/portfolio/static/media/journie-morning.cfd5754b.gif"
+  },
+  {
+    "revision": "d68430c09771c3a0822aae9d0f0aad95",
+    "url": "/portfolio/static/media/journie-morning.d68430c0.png"
+  },
+  {
+    "revision": "2c7eb3f8e4a8d1b4cf7b5f05ef2a52c5",
+    "url": "/portfolio/static/media/journie-nits-needs.2c7eb3f8.png"
+  },
+  {
+    "revision": "176ad2991f259f677f9f6a3054a63ecb",
+    "url": "/portfolio/static/media/journie-onboard.176ad299.gif"
+  },
+  {
+    "revision": "fa233cce61606fb14d6442b84a1c93cc",
+    "url": "/portfolio/static/media/journie-onboard.fa233cce.png"
+  },
+  {
+    "revision": "ac6aa4ecb1494edfa11a878eb018cbe8",
+    "url": "/portfolio/static/media/journie-preview.ac6aa4ec.png"
+  },
+  {
+    "revision": "9277a170062059d1ec1f3402569b847e",
+    "url": "/portfolio/static/media/journie-project-preview.9277a170.png"
+  },
+  {
+    "revision": "91a3e4760cde10623f72ff4458919c5e",
+    "url": "/portfolio/static/media/journie-prototype.91a3e476.jpeg"
+  },
+  {
+    "revision": "60f18fba0e82bee43ad610199062e1e8",
+    "url": "/portfolio/static/media/journie-prototype1.60f18fba.png"
+  },
+  {
+    "revision": "1d4772c811a8ecf94bb76674df2a0eab",
+    "url": "/portfolio/static/media/journie-sketches.1d4772c8.jpg"
+  },
+  {
+    "revision": "7ea50d6130ecbce2be19cdcd7a9fbc2c",
+    "url": "/portfolio/static/media/journie-style-tile.7ea50d61.png"
+  },
+  {
+    "revision": "a061f7a18de126d63bfb474713cc51aa",
+    "url": "/portfolio/static/media/journie-user-test.a061f7a1.jpg"
+  },
+  {
+    "revision": "06e5aa764e1aeeed4dfcfc7386ee4c3e",
+    "url": "/portfolio/static/media/journie-wireframes.06e5aa76.png"
+  },
+  {
+    "revision": "5b49e54e42b5f212fb9c74b437216c3b",
+    "url": "/portfolio/static/media/landing.5b49e54e.png"
+  },
+  {
+    "revision": "6c41513febb985683f7cb2d4f8f5b0fe",
+    "url": "/portfolio/static/media/landingwireframe.6c41513f.png"
+  },
+  {
+    "revision": "36e28aacc9db0145181a0f056e9d3634",
+    "url": "/portfolio/static/media/laptopmockup@2x.36e28aac.png"
+  },
+  {
+    "revision": "06bca9df5de99818eb847ff17c9f005b",
+    "url": "/portfolio/static/media/leaflet.06bca9df.png"
+  },
+  {
+    "revision": "d8bfa9b998d8d79f6ba2c7e326582bb2",
+    "url": "/portfolio/static/media/leafletcode.d8bfa9b9.png"
+  },
+  {
+    "revision": "08cee8926611572bdf55c2122b68a3d8",
+    "url": "/portfolio/static/media/leafletcompetitormatrix.08cee892.png"
+  },
+  {
+    "revision": "5369c087221676c7cd7fe6460686c6c0",
+    "url": "/portfolio/static/media/leaflethifi.5369c087.png"
+  },
+  {
+    "revision": "ce4440cde378c33ca142258c124ff114",
+    "url": "/portfolio/static/media/leafletjourneymap.ce4440cd.png"
+  },
+  {
+    "revision": "1260a65c8e0572b372c017c07705b21b",
+    "url": "/portfolio/static/media/leafletlowfi.1260a65c.png"
+  },
+  {
+    "revision": "da2a33409033659e8fd7cbd5df91dc82",
+    "url": "/portfolio/static/media/leafletmockup@2x.da2a3340.png"
+  },
+  {
+    "revision": "2a24ee370db82e6daaa5a497f76ea013",
+    "url": "/portfolio/static/media/leafletsketch.2a24ee37.JPG"
+  },
+  {
+    "revision": "1334ff4116d7a54c6a24e0d2ffe01164",
+    "url": "/portfolio/static/media/newspin.1334ff41.png"
+  },
+  {
+    "revision": "6eecd17098a5b688c76a40fca8187f4f",
+    "url": "/portfolio/static/media/newspinopen.6eecd170.png"
+  },
+  {
+    "revision": "70e2a976d3342991c808674fcf6b91b2",
+    "url": "/portfolio/static/media/notes.70e2a976.png"
+  },
+  {
+    "revision": "961d43a7763c3f14443f48057e767637",
+    "url": "/portfolio/static/media/oldjobsboard.961d43a7.png"
+  },
+  {
+    "revision": "5035080a8a042d3eb275cae4ae564b7c",
+    "url": "/portfolio/static/media/postjob.5035080a.png"
+  },
+  {
+    "revision": "0737810d73abb00860fdfa15fb2ca206",
+    "url": "/portfolio/static/media/quotebank.0737810d.png"
+  },
+  {
+    "revision": "0fdf8267363a61b36e37dfd8850590a7",
+    "url": "/portfolio/static/media/relatednews.0fdf8267.png"
+  },
+  {
+    "revision": "d8c2482649d3eae48cde534a0d7da34f",
+    "url": "/portfolio/static/media/surveygraphic@2x.d8c24826.png"
+  },
+  {
+    "revision": "3057ab2939e07562a2523d1d7cbf6b15",
+    "url": "/portfolio/static/media/test1.3057ab29.png"
+  },
+  {
+    "revision": "a13792ae0f06e855fb6ab65d1c602c53",
+    "url": "/portfolio/static/media/test2.a13792ae.png"
+  },
+  {
+    "revision": "b925d8087a506f8e13b9079d85721ffd",
+    "url": "/portfolio/static/media/texteditor.b925d808.png"
+  },
+  {
+    "revision": "14d8dd5a0dd8897a23ffd1e88b63b325",
+    "url": "/portfolio/static/media/transcription.14d8dd5a.png"
+  },
+  {
+    "revision": "102d5b12da566f6ea9547eebc539f8d6",
+    "url": "/portfolio/static/media/webmdpreview@2x.102d5b12.png"
+  },
+  {
+    "revision": "fda2f4ef7a3e0a0b104b9b08e921a981",
+    "url": "/portfolio/static/media/youngme.fda2f4ef.png"
+  }
+]);
